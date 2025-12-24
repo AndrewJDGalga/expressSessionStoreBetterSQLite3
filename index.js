6c36e8baf61a50faf05e8cf125e973d9d79f7efc
AndrewJDGalga <70071943+AndrewJDGalga@users.noreply.github.com>
2025-12-24 13:23:36 -0600

    func signatures, gonna try even the optional methods. Base implementations probably don't need mod.

--- index.js
+++ index.js
@@ -5,19 +5,6 @@ export {ExpressSessionStore};
 
 class ExpressSessionStore extends session.Store {
     #location;
-    constructor(options) {
-        super(options);
-        this.#location = options.dbPath || './sessions.db';
-        const db = this.#dbConn(this.#location, (err)=>{ throw err});
-        db.exec(`
-            create table if not exists sessions (
-                sid text primary key,
-                sess text not null,
-                expire integer not null
-            );
-        `);
-        db.close();
-    }
     #dbConn(location, callback){
         let db = null;
         try {
@@ -28,49 +15,38 @@ class ExpressSessionStore extends session.Store {
         return db;
     }
 
-    /*
-    regenerate(req: express.Request, callback: (err?: any) => any): void;
+    constructor(options) {
+        super(options);
+        this.#location = options.dbPath || './sessions.db';
+        const db = this.#dbConn(this.#location, (err)=>{ throw err});
+        db.exec(`
+            create table if not exists express-session (
+                sid text primary key,
+                sess text not null,
+                expire integer not null
+            );
+        `);
+        db.close();
+    }
+    get(sid, callback=null){
 
-    load(sid: string, callback: (err: any, session?: SessionData) => any): void;
+    }
+    set(sid, sessionData, callback=null){
 
-    createSession(req: express.Request, session: SessionData): Session & SessionData;
-    */
-    /**
-     * Gets the session from the store given a session ID and passes it to `callback`.
-     *
-     * The `session` argument should be a `Session` object if found, otherwise `null` or `undefined` if the session was not found and there was no error.
-     * A special case is made when `error.code === 'ENOENT'` to act like `callback(null, null)`.
-     */
-    //abstract get(sid: string, callback: (err: any, session?: SessionData | null) => void): void;
+    }
+    destroy(sid, callback=null){
 
-    /** Upsert a session in the store given a session ID and `SessionData` */
-    //abstract set(sid: string, session: SessionData, callback?: (err?: any) => void): void;
+    }
+    all(callback=null) {
 
-    /** Destroys the session with the given session ID. */
-    //abstract destroy(sid: string, callback?: (err?: any) => void): void;
+    }
+    length(callback=null){
 
-    /** Returns all sessions in the store */
-    // https://github.com/DefinitelyTyped/DefinitelyTyped/pull/38783, https://github.com/expressjs/session/pull/700#issuecomment-540855551
-    //all?(callback: (err: any, obj?: SessionData[] | { [sid: string]: SessionData } | null) => void): void;
+    }
+    clear(callback=null){
 
-    /** Returns the amount of sessions in the store. */
-    //length?(callback: (err: any, length?: number) => void): void;
+    }
+    touch(sid, sessionData, callback=null){
 
-    /** Delete all sessions from the store. */
-    //clear?(callback?: (err?: any) => void): void;
-
-    /** "Touches" a given session, resetting the idle timer. */
-    //touch?(sid: string, session: SessionData, callback?: () => void): void;
-}
-
-/**
- * Get connection to SQlite3 DB, and creates if not present.
- * @access private
- * 
- * @returns {(Database | null)}
- */
-function dbConnection(location) {
-    let db = false;
-    
-    return db;
+    }
 }
